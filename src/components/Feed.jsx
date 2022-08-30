981733b46c9ac47efaa63ed07e7878bb9a45606c
RDBagwell <erbagwell@yahoo.com>
2022-08-29 22:30:23 -0600

    Setup

---
+++ src/components/Feed.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+const Feed = () => {
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row"}}}></Stack>
+  )
+}
+
+export default Feed
