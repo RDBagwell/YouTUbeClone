ebee609e6c48001dc656291578647aed74b070f0
Robert B <robertb@lifesciencepublishers.com>
2022-08-30 15:52:38 -0600

    Card Styling

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { Typography, Card, CardContent, CardMedia } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
-import { demoThumbnailUrl, demoChannelUrl, demoVideoUrl, demoChannelTitle  } from '../utils/constants';
+import { demoThumbnailUrl, demoChannelUrl, demoVideoUrl, demoChannelTitle, demoVideoTitle  } from '../utils/constants';
 
 const VideoCard = ({video: {id:{videoId},  snippet}}) => {
   return (
-    <Card>
+    <Card sx={{ 
+      width: {md: '320px', xs: '100%'},
+      boxShadow: 'none',
+      borderRadius: 0
+      }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia 
         image={snippet?.thumbnails?.high?.url}
@@ -14,6 +18,19 @@ const VideoCard = ({video: {id:{videoId},  snippet}}) => {
         sx={{ width: 358, height: 180}}
         />
       </Link>
+      <CardContent sx={{ background: '#1e1e1e', height: '106px'}} >
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant='subtitle1' fontWeight='bold' color='white'>
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+          <Typography variant='subtitle2' fontWeight='bold' color='gray'>
+            {snippet?.channelTitle || demoChannelTitle }
+            <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }} />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   )
 }
