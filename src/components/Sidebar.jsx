a6206fff0a798140699a4a87cc301c823567f5b3
Robert B <robertb@lifesciencepublishers.com>
2022-08-31 16:17:04 -0600

    Code Cleanup

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Stack } from '@mui/material';
 import { categories } from '../utils/constants';
-import { margin } from '@mui/system';
 
 const Sidebar = ({ selectedCategory, setSelectedCategory }) =>  (
     <Stack
