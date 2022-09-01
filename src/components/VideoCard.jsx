91cc9c77fd2dbc0537b81e9cd329ed04d48b12fa
Robert B <robertb@lifesciencepublishers.com>
2022-09-01 11:19:00 -0600

    Code Cleanup

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -7,7 +7,7 @@ import { demoChannelUrl, demoVideoUrl, demoChannelTitle, demoVideoTitle  } from 
 const VideoCard = ({video: {id:{videoId},  snippet}}) => {
   return (
     <Card sx={{ 
-      width: {md: '320px', xs: '100%'},
+      width: {md: '320px', sm: '358px', xs: '100%'},
       boxShadow: 'none',
       borderRadius: 0
       }}>
@@ -15,7 +15,7 @@ const VideoCard = ({video: {id:{videoId},  snippet}}) => {
         <CardMedia 
         image={snippet?.thumbnails?.high?.url}
         alt={snippet?.title}
-        sx={{ width: 358, height: 180}}
+        sx={{ width: { xs: '100%', sm: '358px', md: '320px'}, height: 180 }}
         />
       </Link>
       <CardContent sx={{ background: '#1e1e1e', height: '106px'}} >
